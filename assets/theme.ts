ba0294d612d9b9b2aaa06d3d2aa801da4f9de070
Carlos Gonzalez <carlossgv@gmail.com>
2022-04-17 11:05:52 -0400

    Created theme file

---
+++ assets/theme.ts
@@ -0,0 +1,11 @@
+const myTheme = {
+  colors: {
+    accentColor: '#FFAF29',
+    backgroundPrimaryColor: '#111319',
+    backgroundSecondaryColor: '#191C26',
+    sidebarBackgroundColor: '#080A12',
+    font: '#FFFFFF',
+  },
+};
+
+export default myTheme;
